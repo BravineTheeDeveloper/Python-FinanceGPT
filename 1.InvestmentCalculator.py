a3712c3d9e4b4e4c48b7c01d2be0833779992c4b
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-04-20 18:09:15 +0300

    Investment Calculator

---
+++ 1.InvestmentCalculator.py
@@ -0,0 +1,13 @@
+# Input initial investment amount, interest rate, and time horizon
+initial_investment = 10000
+interest_rate = 0.05
+time_horizon = 5
+
+# Calculate future value of the investment using compound interest formula
+future_value = initial_investment * (1 + interest_rate) ** time_horizon
+
+# Display future value of the investment
+print(f'The future value of your investment after {time_horizon} years is: ${future_value:.2f}')
+
+# Provide functionalities such as working with user inputs and basic financial calculations
+# ... (implement additional functionalities as needed)
